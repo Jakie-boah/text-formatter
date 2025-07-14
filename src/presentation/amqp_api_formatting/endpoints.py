9ee5f5e9566e2125fecfa680ceb49a59b384d365
slonn <vanogalen@yandex.ru>
2025-07-14 14:10:23 +0300

    feat: дополнил ендпоинт - в целом можнео потестировать

--- src/presentation/amqp_api_formatting/endpoints.py
+++ src/presentation/amqp_api_formatting/endpoints.py
@@ -1,24 +1,20 @@
-from dishka import FromDishka
-from dishka.integrations.faststream import inject
 from faststream.rabbit import RabbitMessage, RabbitQueue, RabbitRouter, RabbitBroker
 from faststream.rabbit.schemas.queue import QueueType
 from loguru import logger
-from pydantic import BaseModel, Field, NonNegativeInt
+from dishka import FromDishka
+from dishka.integrations.faststream import inject
+from src.domain.entities.common import TaskExecutionData
+from src.application.interactors.text_format_interactor import TextInteractor
 
 endpoints = RabbitRouter(include_in_schema=True)
 
 
-class UserInfo(BaseModel):
-    name: str = Field(..., examples=["John"], description="Registered user name")
-    user_id: NonNegativeInt = Field(..., examples=[1], description="Registered user id")
-
-
 @endpoints.subscriber(
     queue=RabbitQueue(
-        name="format_text",
+        name="himmera_format_text",
         durable=True,
         passive=False,
-        routing_key="format_text",
+        routing_key="himmera_format_text",
         queue_type=QueueType.QUORUM,
     ),
     no_ack=False,
@@ -26,12 +22,20 @@ class UserInfo(BaseModel):
 )
 @inject
 async def format_text(
-    user: UserInfo,
+    payload: TaskExecutionData,
     msg: RabbitMessage,
+    interactor: FromDishka[TextInteractor],
     broker: FromDishka[RabbitBroker],
 ):
-    logger.info("ЙОУ")
-    logger.info(user)
-    assert user.name == "John"
-    assert user.user_id == 1
-    await broker.publish(message="etsttetttte kdsfsd", queue="publish_post")
+    logger.info(
+        f"получил задачу на форматирование текста для Feed(id={payload.feed_id}, user_id={payload.user_id})"
+    )
+
+    text = await interactor(payload)
+    logger.info(f"Выполнено форматирование - полученный текст\n\n{text}")
+
+    formatting_result = {"text": text}
+    payload.formatting_result = formatting_result
+
+    await broker.publish(payload.model_dump(), queue="himmera_publish_post")
+    await msg.ack()
